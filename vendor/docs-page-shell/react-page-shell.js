104a92cb7c0d13be1be96381752738a737e98adc
Katy DeCorah <katydecorah@gmail.com>
2020-08-25 13:49:24 -0400

    Update page shell

--- vendor/docs-page-shell/react-page-shell.js
+++ vendor/docs-page-shell/react-page-shell.js
@@ -1089,9 +1089,9 @@ exports.titleGenerator = titleGenerator;
 
 function titleGenerator(title, subsite, site) {
   // create array for formatted title: {title} | {subsite} | {site}
-  var titleArr = []; // do not push a title that is "Introduction"
+  var titleArr = []; // do not push a title that is "Introduction" or "Overview"
 
-  if (title && title !== 'Introduction' && (subsite || site)) titleArr.push(title); // push subsite, if same value doesn't exist yet, strip "for (Product)" from name
+  if (title && title !== 'Introduction' && title !== 'Overview' && (subsite || site)) titleArr.push(title); // push subsite, if same value doesn't exist yet, strip "for (Product)" from name
 
   if (subsite && titleArr.indexOf(subsite) === -1) titleArr.push(subsite.replace(/\sfor\s(iOS|Android|Vision|Unity)/, '')); // push site, if same value doesn't exist yet
 
